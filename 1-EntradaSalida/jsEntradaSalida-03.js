c7017d3269a4bb5c021bde57bd30ed4341a9770f
Pablo <pablocossio133@gmail.com>
2022-02-01 19:34:57 -0300

    nuevo cambio test

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,21 @@
 /*
-Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+	Debemos lograr tomar un dato por 'ID' y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'
+	Nombre programador: Pablo omar cossio
+	Lenguaje: Javascript
+	fecha: 1/01/2022
+	info: Archivo js en donde se ejecuta un saludo.
+*/
+
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	// var nombreIngresado;
+	// Llamamos al id del input desde html a js, utilizando el objeto .value obtendremos el valor.
+	// nombreIngresado= txtIdNombre.value;
+	// alert(nombreIngresado);
+
+	// other example
+	var user_value;
+	user_value= document.getElementById('txtIdNombre').value;
+	alert(user_value);
 
 }
-
-
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,17 @@
 /* 
-Debemos lograr tomar un dato por 'PROMPT' 
-y lo muestro por 'ID' al presionar el botón 'mostrar'
+	Debemos lograr tomar un dato por 'PROMPT' y lo muestro por 'ID' al presionar el botón 'mostrar' 
+	Nombre programador: Pablo omar cossio
+	Lenguaje: Javascript
+	fecha: 1/01/2022
+	info: Archivo js en donde se ejecuta un prompt para ingresar un valor al oprimir el botón mostrar y mostrarlo en el input.
 */
-function mostrar()
+	function mostrar()
 {
-	alert("ok");
+	var nombreUsuario;
+	console.log("antes del dato");
+	nombreUsuario= prompt("Ingrese un valor :");
+	console.log("déspues del dato");
+	document.getElementById('txtIdNombre').value= nombreUsuario;
+
 }
 
