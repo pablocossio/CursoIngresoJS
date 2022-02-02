e2a067efc082ff56334dfc5869d46fe6ecc4cced
Pablo <pablocossio133@gmail.com>
2022-02-02 09:30:07 -0300

    nuevo modificación test

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,18 @@
 /*
-Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
+	Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados  ej: "Usted se llama José y tiene 66 años" 	
+	Nombre programador: Pablo omar cossio
+	Lenguaje: Javascript
+	fecha: 1/01/2022
+	info: Archivo js en donde se ejecuta pedir nombre y edad y concatenar los los valores en una varaible y mostrar por alert.
+*/
+
 function mostrar()
 {	
-	alert("ok");
+	var nombre;
+	var edad;
+	var resultado;
+	nombre= txtIdNombre.value;
+	edad= txtIdEdad.value;
+	resultado= "Usted se llama " + nombre + " y tiene " + edad + " años.";
+	alert(resultado);
 }
-
